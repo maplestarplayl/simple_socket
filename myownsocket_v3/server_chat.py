3e10d6a95f280efdb26ad25325ce1aa0aac6e352
LiFeng <maplestarplay@gmail.com>
2023-09-15 10:42:56 +0800

    V3.5:Complete the feat to use GUI on the server side

--- myownsocket_v3/server_chat.py
+++ myownsocket_v3/server_chat.py
@@ -1,4 +1,5 @@
 import PySimpleGUI as sg
+import server_cython as sc
 
 '''
 A simple send/response chat window.  Add call to your send-routine and print the response
@@ -11,7 +12,8 @@ layout = [[sg.Text('Your output will go here', size=(40, 1))],
           [sg.Output(size=(110, 20), font=('Helvetica 10'))],
           [sg.Multiline(size=(70, 5), enter_submits=False, key='-QUERY-', do_not_clear=False),
            sg.Button('SEND', button_color=(sg.YELLOWS[0], sg.BLUES[0]), bind_return_key=True),
-           sg.Button('EXIT', button_color=(sg.YELLOWS[0], sg.GREENS[0]))]]
+           sg.Button('EXIT', button_color=(sg.YELLOWS[0], sg.GREENS[0])),
+           sg.Button('Connect',button_color=(sg.YELLOWS[0],sg.BLUES[0]))]]
 
 window = sg.Window('Chat window', layout, font=('Helvetica', ' 13'), default_button_element_size=(8,2), use_default_focus=False)
 
@@ -19,9 +21,16 @@ while True:     # The Event Loop
     event, value = window.read()
     if event in (sg.WIN_CLOSED, 'EXIT'):            # quit if exit button or X
         break
+    if event == 'Connect':                          # Start the server socket
+        py_socket_arr = sc.init_socket()
+        socket_fd = py_socket_arr[0]
+        new_socket_fd = py_socket_arr[1]
     if event == 'SEND':
-        query = value['-QUERY-'].rstrip()
+        send_msg = value['-QUERY-'].rstrip()
+        buffer_send_msg = send_msg.encode()
         # EXECUTE YOUR COMMAND HERE
-        print('The command you entered was {}'.format(query), flush=True)
+        print('The message you send was {}'.format(send_msg), flush=True)
+        receive_msg = sc.handle_communication(new_socket_fd,buffer_send_msg)
+        print('The message you get was{}'.format(receive_msg.decode()),flush=True)
 
 window.close()
