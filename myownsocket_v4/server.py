edbb21039d4b54541f6f99199090cb746088b161
Li Feng <maplestarplay@outlook.com>
2023-09-19 02:57:00 +0000

    Not able to communicate async

--- myownsocket_v4/server.py
+++ myownsocket_v4/server.py
@@ -5,7 +5,8 @@ async def handle_client(reader, writer):
     request = None
     while request != 'quit':
         request = (await reader.read(255)).decode('utf8')
-        response = str(eval(request)) + '\n'
+        print(f"Received {request!r}")
+        response = input("Input what you want to send as server")
         writer.write(response.encode('utf8'))
         await writer.drain()
     writer.close()
